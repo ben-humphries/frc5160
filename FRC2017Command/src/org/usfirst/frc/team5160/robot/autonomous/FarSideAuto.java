5d9225a754ff5c23bbdd275cc80c8432bc1477b8
ben-humphries <ben_humphries@caryacademy.org>
2017-03-29 17:10:51 -0400

    Modified FarSideAuto
    Testing side auto in FarSideAuto. When testing, be ready to disable.
    Unpredictable.

--- FRC2017Command/src/org/usfirst/frc/team5160/robot/autonomous/FarSideAuto.java
+++ FRC2017Command/src/org/usfirst/frc/team5160/robot/autonomous/FarSideAuto.java
@@ -12,6 +12,8 @@ public class FarSideAuto extends CommandGroup{
 	public FarSideAuto(){
 		double dm = Robot.BASE.inchToEncoderTick(1);
 		int multiplier = Robot.autoColorMultiplier(); //Multiplier for rotation
-		addSequential(new CMDAutoTankDrive(dm*100,0,0.03),2.5);
+		addSequential(new CMDAutoTankDrive(dm*64*80,0,1),1.45);
+		addSequential(new CMDAutoRotate(-60, 1));
+		addSequential(new CMDAutoTankDrive(dm*64*80, 0, 1));
 	}
 }
